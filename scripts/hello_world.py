ec3d36bf280c35e1420c41f73d7a05f3b8d1ab26
itaykirshner <45193691+itaykirshner@users.noreply.github.com>
2024-05-03 17:46:54 +0300

    Update hello_world.py

--- scripts/hello_world.py
+++ scripts/hello_world.py
@@ -3,12 +3,12 @@
 import sys
 sys.path.append("..")
 
-def hello_world():
+def hello_world(*args, **kwargs):
     from common.create_logger import create_logger
   
     logger = create_logger("hello_world")
     main(logger)
 
 
-def main(logger):
+def main(logger, *args, **kwargs):
     logger.info("Hello World!")
